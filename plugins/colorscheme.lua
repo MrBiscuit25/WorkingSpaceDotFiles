6db4b27e4e5d48eeb1a340360fcae81fd3d1b9cb
Dmitrii <dmitrii1997033@gmail.com>
2024-04-24 15:27:05 +0300

    adding tmux-nvim-navigator in tmux and neovim to seamlessly move between panes

--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -14,85 +14,85 @@
 --     }
 --   end,
 -- }
-return {
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      transparent_background = true,
-      backgound = {
-        light = "latte",
-        dark = "frappe",
-      },
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-          inlay_hints = {
-            backgound = true,
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-}
 -- return {
 --   {
---     "craftzdog/solarized-osaka.nvim",
---     branch = "osaka",
+--     "catppuccin/nvim",
 --     lazy = true,
---     priority = 1000,
---     opts = function()
---       return {
---         transparent = true,
---         terminal_colors = true,
---         styles = {
---           sidebars = "transparent",
---           floats = "dark",
+--     name = "catppuccin",
+--     opts = {
+--       transparent_background = true,
+--       backgound = {
+--         light = "latte",
+--         dark = "frappe",
+--       },
+--       integrations = {
+--         aerial = true,
+--         alpha = true,
+--         cmp = true,
+--         dashboard = true,
+--         flash = true,
+--         gitsigns = true,
+--         headlines = true,
+--         illuminate = true,
+--         indent_blankline = { enabled = true },
+--         leap = true,
+--         lsp_trouble = true,
+--         mason = true,
+--         markdown = true,
+--         mini = true,
+--         native_lsp = {
+--           enabled = true,
+--           underlines = {
+--             errors = { "undercurl" },
+--             hints = { "undercurl" },
+--             warnings = { "undercurl" },
+--             information = { "undercurl" },
+--           },
+--           inlay_hints = {
+--             backgound = true,
+--           },
 --         },
---       }
---     end,
+--         navic = { enabled = true, custom_bg = "lualine" },
+--         neotest = true,
+--         neotree = true,
+--         noice = true,
+--         notify = true,
+--         semantic_tokens = true,
+--         telescope = true,
+--         treesitter = true,
+--         treesitter_context = true,
+--         which_key = true,
+--       },
+--     },
 --   },
 --   {
 --     "LazyVim/LazyVim",
 --     opts = {
---       colorscheme = "solarized-osaka",
+--       colorscheme = "catppuccin",
 --     },
 --   },
 -- }
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "osaka",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "dark",
+        },
+      }
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -14,6 +14,8 @@ return {
           "emmet_ls",
           "lua_ls",
           "prismals",
+          "ruby_lsp",
+          "solograph",
         },
       })
     end,
@@ -44,6 +46,7 @@ return {
           "sass",
           "scss",
           "typescriptreact",
+          "eruby",
         },
       })
       lspconfig.prismals.setup({
@@ -52,10 +55,18 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.solograph.setup({
+        capabilities = capabilities,
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
