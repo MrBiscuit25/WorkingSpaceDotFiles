8de4b6e31dfd0574b8ad71562d087d8c24a800d3
MrBiscuit25 <87424786+MrBiscuit25@users.noreply.github.com>
2024-04-24 15:47:02 +0300

    Update keymaps.lua

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -5,7 +5,3 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "<c-k>", ":wincmd k<CR>")
-keymap.set("n", "<c-j>", ":wincmd j<CR>")
-keymap.set("n", "<c-h>", ":wincmd h<CR>")
-keymap.set("n", "<c-l>", ":wincmd l<CR>")
